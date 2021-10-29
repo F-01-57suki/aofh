9c94106e8887e4fd26fb26eab7f5add3492f12f3
お湯 <89096216+F-01-57suki@users.noreply.github.com>
2021-10-30 06:55:24 +0900

    マップID3の追加準備

---
+++ story/in_m3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>AVACHIofHORROR（仮）</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  </head>
+  <body id="intro" class="map3">
+    <div id="wrapper">
+      <header>
+      <h1>未実装マップ</h1>
+      </header>
+      <main>
+          <p>現在、未実装のマップです。startからセーブ破棄して下さい。<br><a href="index.php">TOPへ戻る</a></p>
+      </main>
+      <footer>
+        <p>copyright &copy; <?php echo date('Y'); ?> Miyashita.</p>
+      </footer>
+    </div>
+    <?php require_once "tmp/ng_js.php"; ?>
+  </body>
+</html>
