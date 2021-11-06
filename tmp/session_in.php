7e4bb1019cf4fbc2892d3a36ed585268f208702c
お湯 <89096216+F-01-57suki@users.noreply.github.com>
2021-11-07 07:20:50 +0900

    チュートリアルの実装

--- tmp/session_in.php
+++ tmp/session_in.php
@@ -7,6 +7,10 @@ session_regenerate_id(true);
 if(!isset($_SESSION['username'])):
   header('Location: login.php');
 endif;
+//チュートリアル確認
+if($_SESSION['news'] == 'tutorial'):
+  header('Location: tutorial.php');
+endif;
 //エラー配列の作成
 $errors = array();
 ?>
