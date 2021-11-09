d2596c9a3021cebd8aabe7a4d3caf965b5272408
お湯 <89096216+F-01-57suki@users.noreply.github.com>
2021-11-09 09:53:08 +0900

    仮テキストを本テキストに差し替え

--- story/in_m2.php
+++ story/in_m2.php
@@ -3,21 +3,25 @@
   <head>
     <meta charset="UTF-8">
     <title>AVACHIofHORROR（仮）</title>
-    <link href="style.css" rel="stylesheet">
+    <link href="../style.css" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   </head>
   <body id="intro" class="map2">
     <div id="wrapper">
       <header>
-      <h1>未実装マップ</h1>
+      <h1>COMING SOON...</h1>
       </header>
       <main>
-          <p>現在、未実装のマップです。startからセーブ破棄して下さい。<br><a href="index.php">TOPへ戻る</a></p>
+        <div style="text-align: center;">
+          <p style="text-align: center;">現在、未実装のマップです</p>
+          <img src="../images/comingsoon.png" alt="工事中">
+          <a href="../index.php"><img src="../images/topbtn.png" alt="TOPへ戻る"></a>
+        </div>
       </main>
       <footer>
         <p>copyright &copy; <?php echo date('Y'); ?> Miyashita.</p>
       </footer>
     </div>
-    <?php require_once "tmp/ng_js.php"; ?>
+    <?php require_once "../tmp/ng_js.php"; ?>
   </body>
 </html>
